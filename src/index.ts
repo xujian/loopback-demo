1483c2c6e89b2b44d24a3867095e695a98df4a01
xujian <782135@qq.com>
2019-12-02 11:37:17 +0800

    init commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {LoopBackDemoApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {LoopBackDemoApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new LoopBackDemoApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
